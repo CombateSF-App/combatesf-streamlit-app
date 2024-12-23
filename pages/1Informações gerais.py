b26ec1be94e083e20e0adf944e692cebe930963e
Ayana Tanaka <ayana.tanaka.1401@gmail.com>
2024-12-23 16:08:48 -0300

    Erros corrigidos

--- pages/1Informações gerais.py
+++ pages/1Informações gerais.py
@@ -19,9 +19,9 @@ st.set_page_config(page_title="Informações Gerais", layout="wide")
 #Barra superior com logos
 col1, col2, col3, col4 = st.columns([3, 2, 1, 3])
 with col2:
-    st.image("logos\\logotipo_combate.png")
+    st.image("logos/logotipo_combate.png")
 with col3:
-    st.image("logos\\logotipo_Maxsatt.png")
+    st.image("logos/logotipo_Maxsatt.png")
 
 
 st.title("Visão geral")
@@ -30,7 +30,7 @@ st.title("Visão geral")
 def connect(file):
     return duckdb.connect(file)
 conn = connect('my_database.db')
-conn.execute("CREATE TABLE IF NOT EXISTS pred_attack AS SELECT * FROM 'prediction\\pred_attack_2024.parquet'")
+conn.execute("CREATE TABLE IF NOT EXISTS pred_attack AS SELECT * FROM 'prediction/pred_attack_2024.parquet'")
 query = """
 SELECT * FROM pred_attack
 WHERE DATE = ?
@@ -69,7 +69,7 @@ if st.session_state.selectedvariable3:
     pred_attack['FARM'] = pred_attack['FARM'].str.upper()
     pred_attack['STAND'] = pred_attack['STAND'].str.upper()
 
-    stands_all = gpd.read_file("prediction\\Talhoes_Manulife_2.shp")
+    stands_all = gpd.read_file("prediction/Talhoes_Manulife_2.shp")
     stands_all = stands_all.to_crs(epsg=4326)
     stands_all['COMPANY'] = stands_all['Companhia'].str.upper()
     stands_all['FARM'] = stands_all['Fazenda'].str.replace(" ", "_")
